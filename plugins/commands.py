b55995453565fcf73de1d38fcb88b30b6bdb74b3
Itz Me JEOL <102511853+Jeolpaul@users.noreply.github.com>
2023-01-16 19:58:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -33,11 +33,11 @@ async def start(client, message):
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
     if query.data == "help":
-    await client.send_message(
-        chat_id=message.chat.id,
-        reply_markup=reply_markup,
-        text=Translation.HELP_TXT,
-        parse_mode="html")
+        await client.send_message(
+            chat_id=message.chat.id,
+            reply_markup=reply_markup,
+            text=Translation.HELP_TXT,
+            parse_mode="html")
 
 #=================About Function==================#
 
